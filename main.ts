c36f95adec0875ea02d1272fc25f529bde92ca19
Simon Thorneycroft <simon.thorneycroft@syncadia.com>
2021-01-23 11:02:06 +0000

    Enemies game over

--- main.ts
+++ main.ts
@@ -16,6 +16,57 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Coin, function (sprite, otherSpr
 scene.onOverlapTile(SpriteKind.Player, assets.tile`portal`, function (sprite, location) {
     game.over(true)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    bingo.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    raged_bad_guy.destroy()
+    raged_bad_guy = sprites.create(assets.image`raged hunter`, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    raged_bad_guy,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . f . f f f . f . . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . 2 f f f 2 . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . f f f f f f f f f . . . . . 
+        . f . . f f f f f . . f . . . . 
+        . f . . f f f f f . . f . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . . f f f f . . . . . . . 
+        . . . . . . f f . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . f . f f f . f . . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . f f f f 2 . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f f f f f f f . . f f . . 
+        . . f f f f f f f f f f f . . . 
+        . . f . f f f f f . . . f f . . 
+        . f . . f f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . . f f f f . . . . . . . 
+        . . . . . . f f . . . . . . . . 
+        `],
+    100,
+    true
+    )
+    bingo.setPosition(bingo.x, bingo.y - 80)
+    raged_bad_guy.follow(bingo)
+})
+let raged_bad_guy: Sprite = null
 let coin: Sprite = null
 let bingo: Sprite = null
 bingo = sprites.create(img`
@@ -36,7 +87,7 @@ bingo = sprites.create(img`
     . . . . . e . . . e . . . . . . 
     . . . . . f . . . f . . . . . . 
     `, SpriteKind.Player)
-scene.setBackgroundColor(15)
+scene.setBackgroundColor(12)
 controller.moveSprite(bingo, 100, 0)
 tiles.setTilemap(tilemap`chapter 1the escape`)
 bingo.ay += 200
@@ -51,4 +102,9 @@ for (let value of tiles.getTilesByType(assets.tile`tile0`)) {
     100,
     true
     )
+}
+for (let value of tiles.getTilesByType(assets.tile`tile6`)) {
+    raged_bad_guy = sprites.create(assets.image`hunter`, SpriteKind.Enemy)
+    tiles.placeOnTile(raged_bad_guy, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
 }
